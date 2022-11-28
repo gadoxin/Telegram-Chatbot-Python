8b3c8bb7db818db47dac5da07e16c6e35e6c9a1b
gadoxin <gadoxin807@gmail.com>
2022-11-28 07:24:49 +0100

    Telegram chatbot, this bot allows users to purchase Virtual numbers.

--- Script/main.py
+++ Script/main.py
@@ -12,6 +12,7 @@ import urllib3
 import telepot.api
 proxy_url = 'http://proxy.server:3128'
 
+
 telepot.api._pools = {
     'default': urllib3.ProxyManager(proxy_url=proxy_url, num_pools=3, maxsize=10, retries=False, timeout=30), }
 
